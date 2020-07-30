fe138e4df3d151ef086af2ba92a3ebe7fa066945
Flavia Trevisan <67347200+flatrevisan@users.noreply.github.com>
2020-07-30 19:01:10 -0300

    Add files via upload

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,37 @@
+function identifyLovers() {
+  
+  if (typeof contratoComSignatario === "undefined") {
+    alert("Você não está conectado ao Ethereum. Verifique seu Metamask");
+    return;
+  }
+  contratoComSignatario
+  .identifyLovers($("#_loversInvolved").val(), $("#_party").val(), $("#_name").val())
+  .then((transacao) => {
+    $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
+    $("#statusTransacoes").toggle();
+    transacao
+      .wait()
+      .then((resultado) => {
+        console.log("identifyLovers- o resultado foi ", resultado);
+        if (resultado.status === 1) {
+          $("#descricaoStatusTransacoes").html("Transação executada.");
+        } else {
+          $("#descricaoStatusTransacoes").html("Houve um erro na execução da transação no Ethereum.");
+        }
+      })
+      .catch((err) => {
+        console.error("commitment - a transação foi minerada e houve um erro. Veja abaixo");
+        console.error(err);
+        $("#descricaoStatusTransacoes").html("Algo saiu errado: " + err.message);
+      });
+  })
+  .catch((err) => {
+    console.error("commitment - tx só foi enviada");
+    console.error(err);
+    $("#descricaoStatusTransacoes").html("Algo saiu errado antes de enviar ao Ethereum: " + err.message);
+  });
+}
+
 function commitment() {
   
     if (typeof contratoComSignatario === "undefined") {
@@ -35,4 +69,4 @@ function commitment() {
       console.error(err);
       $("#descricaoStatusTransacoes").html("Algo saiu errado antes de enviar ao Ethereum: " + err.message);
     });
-}
+  }
