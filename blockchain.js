6d9532eb796d09b368f51608f8f084ab4fe62b5c
Flavia Trevisan <67347200+flatrevisan@users.noreply.github.com>
2020-07-30 14:52:42 -0300

    Add files via upload

---
+++ blockchain.js
@@ -0,0 +1,38 @@
+function commitment() {
+  
+    if (typeof contratoComSignatario === "undefined") {
+      alert("Você não está conectado ao Ethereum. Verifique seu Metamask");
+      return;
+    }
+
+    var additionalSettings = {
+        value: ethers.utils.parseUnits('500', 'wei')
+    };
+
+    contratoComSignatario
+    .commitment($("#_loverId").val(), $("#_consent1").val(), $("#_consent2").val(), additionalSettings)
+    .then((transacao) => {
+      $("#descricaoStatusTransacoes").html("Transação enviada. Aguarde pela mineração...");
+      $("#statusTransacoes").toggle();
+      transacao
+        .wait()
+        .then((resultado) => {
+          console.log("commitment - o resultado foi ", resultado);
+          if (resultado.status === 1) {
+            $("#descricaoStatusTransacoes").html("Transação executada.");
+          } else {
+            $("#descricaoStatusTransacoes").html("Houve um erro na execução da transação no Ethereum.");
+          }
+        })
+        .catch((err) => {
+          console.error("commitment - a transação foi minerada e houve um erro. Veja abaixo");
+          console.error(err);
+          $("#descricaoStatusTransacoes").html("Algo saiu errado: " + err.message);
+        });
+    })
+    .catch((err) => {
+      console.error("commitment - tx só foi enviada");
+      console.error(err);
+      $("#descricaoStatusTransacoes").html("Algo saiu errado antes de enviar ao Ethereum: " + err.message);
+    });
+}
---
+++ dados-contratos-inteligentes.js
@@ -0,0 +1,208 @@
+var enderecoContrato = "0x87A9116ff1D727E8dfE3f997A1A64E77ca4Ab75a";
+var abiContrato = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "changeStatusFailed",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_loverId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_consent1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_consent2",
+				"type": "uint256"
+			}
+		],
+		"name": "commitment",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "consentDeclarationIssued",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "feesReceived",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_loversInvolved",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "address payable",
+				"name": "_party",
+				"type": "address"
+			}
+		],
+		"name": "identifyLovers",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "payFee",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "enum RelationshipContract.statuses",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"name": "statusCommitment",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "consent1",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "consent2",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fees",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lawyer",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "loversInvolved",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
